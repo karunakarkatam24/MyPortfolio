fda0903701d82d1c4e002e8caf062b66e9920dc2
2100030993_karunakar <2100030993@kluniversity.in>
2024-09-17 19:10:55 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+tailwind.config={
+    theme:{
+       fontFamily:{
+        Outfit:["Outfit", "sans-serif"],
+        Ovo:["Ovo", "serif"]
+       },
+       animation:{
+        spin_slow:'spin 6s linear infinite'
+       },
+       darkMode:'selector',
+       darkTheme:'#11001F'
+    }
+    }
